a33078b413f86bcf8c23c9f49a6b8adf33f8caa4
JanPanjan <89231282@student.upr.si>
2026-05-01 20:59:53 +0200

    sword2 primer

---
+++ sword2_example.sh
@@ -0,0 +1,25 @@
+#!/bin/fish
+set protein "1a62.pdb"
+
+# dobi ID od trenutnega proteina
+set protein_id (basename "$protein" .pdb)
+echo $protein_id
+#> 1a62
+
+# najdi vnos proteina v seznamu baze
+set db_entry (grep "$protein_id" "$ROOT/"atlas_db/2024_11_18_ATLAS_pdb.txt)
+echo $db_entry
+#> 1a62_A
+
+# dobi uporabljeno verigo
+set chain (string split "_" "$db_entry" -f 2)
+echo $chain
+#> A
+
+# sword2 je wrapper script, ga bom vseeno priložil
+#> #!/bin/bash
+#> # move to sword's directory prior to execution and run it from there
+#> pushd /home/jan/Programs/SWORD2
+#> exec /home/jan/Programs/miniconda3/envs/sword2/bin/python /home/jan/Programs/SWORD2/SWORD2.py "$@"
+#> popd
+sword2 -i "$DB/$protein" -o "$SWO" -c "$chain"
