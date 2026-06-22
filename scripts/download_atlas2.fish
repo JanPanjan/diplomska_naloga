e8191a688eab3b554263b17ad0a18969f571bbb4
JanPanjan <89231282@student.upr.si>
2026-06-22 16:32:18 +0200

    rmsf datoteke

--- scripts/download_atlas2.fish
+++ scripts/download_atlas2.fish
@@ -11,6 +11,8 @@ set data_dir "analysis"
 
 test -d $data_dir || mkdir -p $data_dir
 
+# -P: število istočasnih procesov
+# -I: placeholder za input
 cat "$prot_list" | xargs -P "$n" -I {} curl -X "GET" "$url/{}" -H "$header" -o "$data_dir/{}.zip"
 
 popd
