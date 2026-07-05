edb851240dd9e514bcf34b4e026c09d7537bdaf6
JanPanjan <89231282@student.upr.si>
2026-07-05 19:30:44 +0200

    gmx distances

--- scripts/rmsf_analysis.r
+++ scripts/rmsf_analysis.r
@@ -2,43 +2,36 @@ library(dplyr)
 library(ggplot2)
 
 # iskanje rmsf datotek izbranih proteinov
-files <- list.files("atlas_db/RMSF", pattern = "tsv", full.names = TRUE)
+files <- list.files("../atlas_db/RMSF", pattern = "tsv", full.names = TRUE)
+# files <- list.files("atlas_db/RMSF", pattern = "tsv", full.names = TRUE)
 
 # glavni df iz prejšnje analize
-filtered <- read.csv("sword_results_clean.csv") |> as_tibble()
+# odstrani 3-domenske
+filtered <- read.csv("../sword_results_clean.csv") |>
+    # filtered <- read.csv("sword_results_clean.csv") |>
+    as_tibble() |>
+    group_by(protein) |>
+    filter(max(domain) < 3)
 
 filtered_proteins <- unique(filtered$protein)
 inds <- sapply(filtered_proteins, \(x) grep(x, files))
-
-rmsf_filtered <- files[inds]
+rmsf_filtered_files <- files[inds]
 
 # preverjanje distribucij rmsf vrednosti
-plots <- list()
+rmsf <- read.delim("../atlas_db/RMSF/1rrm_A_RMSF.tsv")
 
-sapply(sample(rmsf_filtered, 10), \(x) {
-    plots[[x]] <<- read.delim(x) |>
-        as_tibble() |>
-        select(RMSF_R1) |>
-        ggplot(aes(x = RMSF_R1)) +
-        geom_histogram(bins = 15)
-})
+par(mfrow = c(1, 2))
+rmsf$RMSF_R1 |> hist()
+log(rmsf$RMSF_R1) |> hist()
+rmsf$RMSF_R1 |> boxplot()
+log(rmsf$RMSF_R1) |> boxplot()
 
-plots[1]
-plots[2]
-plots[3]
-plots[4]
-plots[5]
-plots[6]
-plots[7]
-plots[8]
-plots[9]
-plots[10]
 
 # očitno ne sledijo normalni porazdelitvi
 # zelo so skewed
 
 # velikosti proteinov (število vrstic v RMSF datotekah = št. aminokislin)
-ns <- sapply(rmsf_filtered, \(x) nrow(as_tibble(read.delim(x))))
+ns <- sapply(rmsf_filtered_files, \(x) nrow(as_tibble(read.delim(x))))
 
 summary(ns)
 
@@ -46,25 +39,125 @@ summary(ns)
 
 # lowkey eksponentne distribucije
 # mogoče ...... moram normalizirati na [0,1] ...
-# ker domene niso enako velike. vzorci verjetno
-# morajo biti enako veliki. če normaliziram... še
-# vedno.. nimam enako velikih vzorcev ...
 
 # NOTE: glej zapiske v obsidian
 
-# protein iz profesorjevega grafa, ki ga imam za referenco
+# protein iz profesorjevega grafa, ki ga imam za referenco (ni v filtered!!)
 rmsf <- files[grep("1c96", files)] |> read.delim()
+rmsf <- rmsf[, -1]
 r <- 2
+
 d1 <- rmsf[1:528, r]
 d2 <- rmsf[529:nrow(rmsf), r]
-ks.test(d1, d2)
-#
-#         Asymptotic two-sample Kolmogorov-Smirnov test
-#
-# data:  d1 and d2
-# D = 0.5775, p-value < 2.2e-16
-# alternative hypothesis: two-sided
-#
-# Warning message:
-# In ks.test.default(d1, d2) :
-#   p-value will be approximate in the presence of ties
+test <- ks.test(d1, d2, alternative = "two.sided")
+test$p.value
+
+rmsf <- log(rmsf)
+d1 <- rmsf[1:528, r]
+d2 <- rmsf[529:nrow(rmsf), r]
+test <- t.test(d1, d2, alternative = "two.sided")
+test$p.value
+
+
+# zdaj pa naredi to za vse proteine
+# cilj: dobi rmsf, razdeli po domenah, naredi test, shrani če je valid
+
+prot_group <- function(d, p) {
+    d[d$protein == p, ]
+}
+
+# 1. KS-test
+
+stat_test <- function(d1, d2) {
+    ks.test(d1, d2, alternative = "two.sided")$p.value
+}
+
+# najde rmsf datoteko, podatke o proteinu, izvede test, vrne p-vrednosti testa
+run <- function(protein) {
+    # preberi rmsf
+    rmsf <- files[grep(protein, files)] |> read.delim()
+    rmsf <- rmsf[, -1]
+
+    # dobi podatke od proteina
+    d <- prot_group(filtered, protein)
+
+    pvals <- sapply(names(rmsf), \(col) {
+        # najdi začetek in konec domen
+        start <- d$start
+        end <- d$end
+
+        # matchaj rmsf
+        d1 <- rmsf[start[1]:end[1], col]
+        d2 <- rmsf[start[2]:end[2], col]
+
+        # testiraj
+        stat_test(d1, d2)
+    })
+
+    pvals
+}
+
+#+ warning=FALSE
+test_results <- sapply(filtered_proteins, run) |> t()
+test_results |> head()
+
+# kateri passajo z vsemi replikati
+keep3 <- which(apply(test_results, 1, \(row) all(row < 0.05)))
+
+# kateri passajo z dvema ...
+keep2 <- which(apply(test_results, 1, \(row) sum(row < 0.05) == 2))
+
+# kateri passajo z enim ...
+keep1 <- which(apply(test_results, 1, \(row) any(row < 0.05)))
+
+c("3" = length(keep3), "2" = length(keep2), "1" = length(keep1))
+
+
+# 2. log-transform
+
+stat_test <- function(d1, d2) {
+    t.test(d1, d2, alternative = "two.sided")$p.value
+}
+
+# najde rmsf datoteko, podatke o proteinu, izvede test, vrne p-vrednosti testa
+run <- function(protein) {
+    # preberi rmsf
+    rmsf <- files[grep(protein, files)] |> read.delim()
+    rmsf <- rmsf[, -1]
+
+    # log transform
+    rmsf <- log(rmsf)
+
+    # dobi podatke od proteina
+    d <- prot_group(filtered, protein)
+
+    pvals <- sapply(names(rmsf), \(col) {
+        # najdi začetek in konec domen
+        start <- d$start
+        end <- d$end
+
+        # matchaj rmsf
+        d1 <- rmsf[start[1]:end[1], col]
+        d2 <- rmsf[start[2]:end[2], col]
+
+        # testiraj
+        stat_test(d1, d2)
+    })
+
+    pvals
+}
+
+#+ warning=FALSE
+test_results <- sapply(filtered_proteins, run) |> t()
+test_results |> head()
+
+# kateri passajo z vsemi replikati
+keep3 <- which(apply(test_results, 1, \(row) all(row < 0.05)))
+
+# kateri passajo z dvema ...
+keep2 <- which(apply(test_results, 1, \(row) sum(row < 0.05) == 2))
+
+# kateri passajo z enim ...
+keep1 <- which(apply(test_results, 1, \(row) any(row < 0.05)))
+
+c("3" = length(keep3), "2" = length(keep2), "1" = length(keep1))
